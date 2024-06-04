76b4fb2a056568dd755b4f535807b8f3d98831f1
ahndb <ahndb.dev@gmail.com>
2024-06-04 14:26:56 +0900

    frist commit

---
+++ src/main/java/com/estate/back/common/util/ChangeDateFormatUtil.java
@@ -0,0 +1,32 @@
+package com.estate.back.common.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class ChangeDateFormatUtil {
+  
+  public static String changeYYMMDD(String origninal) throws Exception {
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    Date datetime = simpleDateFormat.parse(origninal);
+    simpleDateFormat = new SimpleDateFormat("yy.MM.dd");
+    String writeDatetime = simpleDateFormat.format(datetime);
+    return writeDatetime;
+  }
+
+  public static String changeYYYYMMDD(String origninal) throws Exception {
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    Date datetime = simpleDateFormat.parse(origninal);
+    simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd");
+    String writeDatetime = simpleDateFormat.format(datetime);
+    return writeDatetime;
+  }
+
+  public static String changeYYMM(String origninal) throws Exception {
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    Date datetime = simpleDateFormat.parse(origninal);
+    simpleDateFormat = new SimpleDateFormat("yy-MM");
+    String writeDatetime = simpleDateFormat.format(datetime);
+    return writeDatetime;
+  }
+
+}
